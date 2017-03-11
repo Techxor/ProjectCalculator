60cf7fd02bae591074175356ed158b62cf136524
DESKTOP-P40N564\Techxor <marcinek.sut@gmail.com>
2017-03-11 01:20:44 +0100

    jj
    hh

---
+++ ProjectCalculator/src/pl/lodz/uni/math/java/Calculator.java
@@ -0,0 +1,8 @@
+package pl.lodz.uni.math.java;
+
+public class Calculator {
+public Calculator() {
+	// TODO Auto-generated constructor stub
+	System.out.println("Siemano");
+}
+}
---
+++ ProjectCalculator/src/pl/lodz/uni/math/java/Main.java
@@ -0,0 +1,10 @@
+package pl.lodz.uni.math.java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Calculator calculator = new Calculator();
+	}
+
+}
